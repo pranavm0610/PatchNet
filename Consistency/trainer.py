ec7db1cc38815650648e1119e025e1e495dec6a0
NikileshJayaguptha <Njayaguptha@gmail.com>
2025-09-09 11:02:02 +0530

    data.py edited

--- Consistency/trainer.py
+++ Consistency/trainer.py
@@ -25,7 +25,7 @@ from scipy.optimize import curve_fit
 from scipy.stats import pearsonr, spearmanr, kendalltau
 import numpy as np
 
-from utils import logistic_func, pearson_loss
+from Consistency.utils import logistic_func, pearson_loss
 
 
 
